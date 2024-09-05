f1cde8e758cf0cbed9a614e8bc500b8ca46d6ec1
jk9112605 <36611144+jk9112605@users.noreply.github.com>
2024-09-05 17:26:50 +0800

    Update Install.bat
    support x64 version

--- Install.bat
+++ Install.bat
@@ -1,21 +1,35 @@
-del %AppData%\Rime\*.* /Q
-rmdir %AppData%\Rime\opencc /S /Q
-xcopy .\Rime\*.* /S %AppData%\Rime\
 @echo off
 setlocal enabledelayedexpansion
 
-set "weaselDir=C:\Program Files (x86)\Rime"
+rem Delete old data
+del %AppData%\Rime\*.* /Q
+rmdir %AppData%\Rime\opencc /S /Q
+xcopy .\Rime\*.* /S %AppData%\Rime\
+
+rem Set folder paths
+set "weaselDir_x86=C:\Program Files (x86)\Rime"
+set "weaselDir_x64=C:\Program Files\Rime"
 set "deployer="
 
-for /d %%d in ("%weaselDir%\weasel-*") do (
+rem Check x86 folder
+for /d %%d in ("%weaselDir_x86%\weasel-*") do (
     if exist "%%d\WeaselDeployer.exe" (
         set "deployer=%%d\WeaselDeployer.exe"
     )
 )
 
+rem If not found in x86 folder, check x64 folder
+if not defined deployer (
+    for /d %%d in ("%weaselDir_x64%\weasel-*") do (
+        if exist "%%d\WeaselDeployer.exe" (
+            set "deployer=%%d\WeaselDeployer.exe"
+        )
+    )
+)
+
+rem Execute deployment
 if defined deployer (
     "%deployer%" /deploy
 ) else (
     echo WeaselDeployer.exe not found.
 )
-
